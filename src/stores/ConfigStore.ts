cde2c2b7ac948be33d0a6affd9a0278c5da16236
shilangyu <xmarcinmarcin@gmail.com>
2019-09-30 23:04:15 +0200

    persistant config

--- src/stores/ConfigStore.ts
+++ src/stores/ConfigStore.ts
@@ -1,11 +1,28 @@
 import { Theme } from 'constants/interfaces'
-import { Language } from 'locales'
-import { action, observable } from 'mobx'
+import { Language } from 'locales/strings'
+import { action, autorun, observable } from 'mobx'
 
 export default class {
 	@observable language: Language = Language.en
 	@observable theme: Theme = 'light'
 
-	@action changeLanguage = (to: Language) => (this.language = to)
-	@action changeTheme = (to: Theme) => (this.theme = to)
+	constructor() {
+		autorun(() => this.save())
+		this.load()
+	}
+
+	@action
+	changeLanguage = (to: Language) => (this.language = to)
+
+	@action
+	changeTheme = (to: Theme) => (this.theme = to)
+
+	save = () =>
+		window.localStorage.setItem(
+			'ConfigStore',
+			JSON.stringify({ language: this.language, theme: this.theme })
+		)
+
+	@action
+	load = () => Object.assign(this, JSON.parse(window.localStorage.getItem('ConfigStore') || '{}'))
 }
