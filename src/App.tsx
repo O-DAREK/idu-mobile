ad8c41c476241c6b872ae6ac0a014de8be562d35
shilangyu <xmarcinmarcin@gmail.com>
2019-09-30 19:10:17 +0200

    created login

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { CssBaseline, MuiThemeProvider } from '@material-ui/core'
+import { StylesProvider } from '@material-ui/styles'
 import Login from 'components/Login'
 import React from 'react'
 import { BrowserRouter, Redirect, Route, Switch } from 'react-router-dom'
@@ -16,12 +17,14 @@ const Router = () => (
 )
 
 const App = () => (
-	<MuiThemeProvider theme={muiTheme}>
-		<>
-			<CssBaseline />
-			<Router />
-		</>
-	</MuiThemeProvider>
+	<StylesProvider injectFirst>
+		<MuiThemeProvider theme={muiTheme}>
+			<>
+				<CssBaseline />
+				<Router />
+			</>
+		</MuiThemeProvider>
+	</StylesProvider>
 )
 
 export default App
--- src/components/Login/index.tsx
+++ src/components/Login/index.tsx
@@ -1,7 +1,35 @@
+import { Button, Grid, Paper, TextField } from '@material-ui/core'
 import React from 'react'
+import styled from 'styled-components'
 
-interface Props {}
+const PaddedGrid = styled(Grid)`
+	padding: 20px;
+`
 
-const Login: React.FC<Props> = () => <div>hello</div>
+const Login: React.FC = () => (
+	<Grid
+		direction="column"
+		alignItems="center"
+		justify="center"
+		style={{ minHeight: '100vh' }}
+		container
+	>
+		<Grid xs={11} item>
+			<Paper>
+				<PaddedGrid direction="column" alignItems="center" justify="center" spacing={1} container>
+					<Grid item>
+						<TextField label="Login" variant="outlined" />
+					</Grid>
+					<Grid item>
+						<TextField label="Password" variant="outlined" />
+					</Grid>
+					<Grid item>
+						<Button variant="outlined">Login </Button>
+					</Grid>
+				</PaddedGrid>
+			</Paper>
+		</Grid>
+	</Grid>
+)
 
 export default Login
