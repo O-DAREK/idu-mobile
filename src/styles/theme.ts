01976c8d33cee07eee22353033feb8446155be25
shilangyu <xmarcinmarcin@gmail.com>
2020-01-09 19:11:45 +0100

    changed theme colors

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,9 +1,14 @@
 import { createMuiTheme } from '@material-ui/core'
+import { blue } from '@material-ui/core/colors'
 import { Theme } from 'constants/interfaces'
 
 export const muiTheme = (themeType: Theme) =>
 	createMuiTheme({
 		palette: {
-			type: themeType
+			type: themeType,
+			primary: blue,
+			secondary: {
+				main: '#ff80ab'
+			}
 		}
 	})
