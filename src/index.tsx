6cfd2c5eae4f9a2e6847daeb0171cb44c042721b
shilangyu <xmarcinmarcin@gmail.com>
2020-01-26 22:08:37 +0100

    hopefully seamless updates? #19

--- src/index.tsx
+++ src/index.tsx
@@ -1,18 +1,18 @@
 import App from 'App'
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { metaStore } from 'stores'
 import * as serviceWorker from './serviceWorker'
 
 ReactDOM.render(<App />, document.getElementById('root'))
 
 serviceWorker.register({
-	onUpdate() {
-		// hacky way to access the context
-		// Nothing will be actually rendered
-		ReactDOM.render(
-			<metaStore.Consumer>{s => <>{s.theresAnUpdate()}</>}</metaStore.Consumer>,
-			document.createElement('noop')
-		)
+	onUpdate(registration) {
+		registration.update()
+		// // hacky way to access the context
+		// // Nothing will be actually rendered
+		// ReactDOM.render(
+		// 	<metaStore.Consumer>{s => <>{s.theresAnUpdate()}</>}</metaStore.Consumer>,
+		// 	document.createElement('noop')
+		// )
 	}
 })
