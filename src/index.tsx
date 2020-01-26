75f0c3588eb20c47c8fe67c4e8c8d27ec6d37132
shilangyu <xmarcinmarcin@gmail.com>
2020-01-26 22:11:39 +0100

    storing update status in metastore

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import App from 'App'
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { metaStore } from 'stores'
 import * as serviceWorker from './serviceWorker'
 
 ReactDOM.render(<App />, document.getElementById('root'))
@@ -8,11 +9,11 @@ ReactDOM.render(<App />, document.getElementById('root'))
 serviceWorker.register({
 	onUpdate(registration) {
 		registration.update()
-		// // hacky way to access the context
-		// // Nothing will be actually rendered
-		// ReactDOM.render(
-		// 	<metaStore.Consumer>{s => <>{s.theresAnUpdate()}</>}</metaStore.Consumer>,
-		// 	document.createElement('noop')
-		// )
+		// hacky way to access the context
+		// Nothing will be actually rendered
+		ReactDOM.render(
+			<metaStore.Consumer>{s => <>{s.theresAnUpdate()}</>}</metaStore.Consumer>,
+			document.createElement('noop')
+		)
 	}
 })
