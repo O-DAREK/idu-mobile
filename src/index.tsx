c606f7d834b962a32cd4ef55068ac698f96f0b62
shilangyu <xmarcinmarcin@gmail.com>
2020-01-31 16:04:09 +0100

    true seamless updates

--- src/index.tsx
+++ src/index.tsx
@@ -8,12 +8,13 @@ ReactDOM.render(<App />, document.getElementById('root'))
 
 serviceWorker.register({
 	onUpdate(registration) {
-		registration.update()
 		// hacky way to access the context
 		// Nothing will be actually rendered
 		ReactDOM.render(
 			<metaStore.Consumer>{s => <>{s.theresAnUpdate()}</>}</metaStore.Consumer>,
 			document.createElement('noop')
 		)
+		// inform workbox to skip waiting, update will be applied after a refresh
+		registration.waiting?.postMessage({ type: 'SKIP_WAITING' })
 	}
 })
