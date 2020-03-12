c8070851b5b9768bc96dec2c761dc2783e3585be
shilangyu <xmarcinmarcin@gmail.com>
2020-03-12 17:23:42 +0100

    changed to position fixed

--- src/components/TopLoading.tsx
+++ src/components/TopLoading.tsx
@@ -2,7 +2,7 @@ import { LinearProgress } from '@material-ui/core'
 import styled from 'styled-components'
 
 const TopLoading = styled(LinearProgress)`
-	position: absolute;
+	position: fixed;
 	top: 0;
 	width: 100vw;
 	z-index: 1000000;
